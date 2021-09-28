31f5064e82f0824a121f66d50693189d929bb047
ravi kant chauhan <ravikantchauhan65@gmail.com>
2021-09-28 17:49:28 +0530

    both commitede

--- diff.java
+++ diff.java
@@ -2,6 +2,6 @@ public class diff {
     public static void main(String[] args) {
         int a = 10;
         int b = 3;
-        System.out.println(a-b);
+        System.out.println("difference of the two given number is: " + a-b);
     }
 }
--- sum.java
+++ sum.java
@@ -3,6 +3,7 @@ public class sum{
     public static void main(String[] args) {
         int a =5;
         int b = 6;
-        System.out.println("sum of the given number is: " + a+b);
+        int c = 10
+        System.out.println("sum of the given number is : " + a+b-c);
     }
 }
