80127f48839079e25a512b3728f50fc63302d77f
ravi kant chauhan <ravikantchauhan65@gmail.com>
2021-09-28 18:38:12 +0530

    ravikant/multiply commited

---
+++ multi.java
@@ -0,0 +1,7 @@
+public class multi {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        System.out.println("multiply of a and b is: " + (a*b));
+    }    
+}
--- sum.java
+++ sum.java
@@ -4,6 +4,6 @@ public class sum{
         int a =5;
         int b = 6;
         int c = 10
-        System.out.println("sum of the given number is : " + a+b-c);
+        System.out.println("sum of numbers is : " + a+b-c);
     }
 }
